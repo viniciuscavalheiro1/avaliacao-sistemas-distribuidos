ecece63db8c3661b19f4cff323fe894ad53a3c91
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-01-29 02:14:21 -0300

    mudança endereço servidor

--- udp_client.py
+++ udp_client.py
@@ -10,7 +10,10 @@ import json # Biblioteca que dá suporte a objetos json
 import time # Biblioteca para gerenciamento de tempo
 import statistics # Módulo para facilitar calculos matematicos
 
-UDP_IP_ADDRESS = "172.17.0.2" #Constante com o endereço IP do Servidor
+UDP_IP_ADDRESS_server = "172.17.0.2"
+UDP_IP_ADDRESS_client = "172.17.0.3"
+
+UDP_IP_ADDRESS =  #Constante com o endereço IP do Servidor
 UDP_PORT_N0 = 4567 # Número da Porta que o Servidor vai ficar escutando
 
 UDP_PORT_N1 = 7654 # Número da Porta que o Cliente vai esperar o retorno
@@ -19,7 +22,7 @@ UDP_PORT_N1 = 7654 # Número da Porta que o Cliente vai esperar o retorno
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Associa o objeto socket acima a uma porta
-clientSock.bind((UDP_IP_ADDRESS, UDP_PORT_N1))
+clientSock.bind((UDP_IP_ADDRESS_client, UDP_PORT_N1))
 
 # Loop de 10 iterações
 times = []
@@ -31,7 +34,7 @@ for x in range(10):
   # Cria  a mensagem com duas informações, a mensagem literal e a porta em que o cliente vai receber o retorno
   message = {"desc": "Olá Servidor, requisição de n° {}".format(x+1) , "porta": UDP_PORT_N1}
   # Envia a mensagem, lembrando que o dicionário acima é convertido em uma string
-  clientSock.sendto(json.dumps(message).encode(), (UDP_IP_ADDRESS, UDP_PORT_N0)) 
+  clientSock.sendto(json.dumps(message).encode(), (UDP_IP_ADDRESS_server, UDP_PORT_N0)) 
 
   # Captura a mensagem e o endereço retornado pelo servidor 
   data, addr = clientSock.recvfrom(1024)
